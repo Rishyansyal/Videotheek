54e0a9b5ba3bfcd95bb5041dea17d262946f191b
Rishyan Syal <karanrishyan@gmail.com>
2025-09-09 15:23:35 +0200

    Werkend
    alleen nog href fixen

--- src/controller/users.controller.js
+++ src/controller/users.controller.js
@@ -1,51 +1,53 @@
-const { Callbacks, error } = require("jquery");
-const userService = require("../service/user.service")
-const logger = require('../util/logger');
+const userService = require("../service/user.service");
+const logger = require("../util/logger");
 const usersDao = require("../dao/users.dao");
-const usersController={
-     get:(req,res,next)=>{
-        let userId = req.params.userId;
-    userService.get(userId,(error,users)=>{
-        if(error) next (error);
-        if (users) { 
-            userId == undefined
-            ? res.render('users/table', {users:users})
-            : res.render('users/details', {user:users[0]}); // krijgt id van die localhost:xxxx/user <-- id wordt ervan meegegeven
-    }
-    })
-    },
-    update:(req,res,next)=>{
-        let userId = req.params.userId;
-        let email = req.body.email;
-        let first_name = req.body.first_name;
-        let last_name = req.    body.last_name;
-        req.method === "GET"
-          ? userService.get(userId, (error, users) => {
-              if (error) next(error);
-              if (users) {
-                userId == undefined
-                  ? res.render("users/table", { users: users })
-                  : res.render("users/edit", { user: users[0] }); // krijgt id van die localhost:xxxx/user <-- id wordt ervan meegegeven
-              }
-            })
-          : userService.update(email, userId, first_name, last_name, (error, result) => {
-              if (error) next(error);
-        if (result) return res.redirect(301,`/users/${userId}/details`);
-        
-    });},
 
-    delete:(req,res,next)=>{
-        let userId = req.params.userId
-        userService.delete(userId,(error,result)=>{
-            if(error) next (error);
-            if (result){
-                res.render("users/users",{users:users})
-            }
-        })
-    },
-    
-    
+const usersController = {
+  get: (req, res, next) => {
+    let userId = req.params.userId;
+    userService.get(userId, (error, users) => {
+      if (error) return next(error);
+
+      if (users) {
+        userId === undefined
+          ? res.render("users/table", { users: users })
+          : res.render("users/details", { user: users[0] }); 
+      }
+    });
+  },
+
+  update: (req, res, next) => {
+    let userId = req.params.userId;
+    let email = req.body.email;
+    let first_name = req.body.first_name;
+    let last_name = req.body.last_name;
+
+    if (req.method === "GET") {
+      userService.get(userId, (error, users) => {
+        if (error) return next(error);
+        if (users) return res.render("users/edit", { user: users[0] });
+      });
+    } else {
+      userService.update(email, userId, first_name, last_name, (error, result) => {
+        if (error) return next(error);
+        if (result) return res.redirect(301, `/users/${userId}/details`);
+      });
+    }
+  },
+
+  delete: (req, res, next) => {
+    let userId = req.params.userId;
+    userService.delete(userId, (error, result) => {
+      if (error) return next(error);
+      if (result) {
+        // hier was 'users' niet gedefinieerd, dus je moet opnieuw alle users ophalen
+        userService.get(undefined, (error, users) => {
+          if (error) return next(error);
+          return res.render("users/users", { users: users });
+        });
+      }
+    });
+  },
 };
 
-
-module.exports = usersController
+module.exports = usersController;
