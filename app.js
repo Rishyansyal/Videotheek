64bb0ad6d33326f6e7e57804cee653173574df7e
Rishyan Syal <karanrishyan@gmail.com>
2025-09-12 17:59:06 +0200

    Add privacy policy route and view; update layout for navigation

--- app.js
+++ app.js
@@ -9,6 +9,7 @@ var indexRouter = require('./src/routes/index.route');
 var usersRouter = require('./src/routes/users.route');
 var authRouter = require('./src/routes/auth.route');
 var moviesRouter = require('./src/routes/movies.route');
+var avg = require("./src/routes/avg.route");
 
 var app = express();
 
@@ -26,6 +27,7 @@ app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/auth', authRouter);
 app.use("/movies", moviesRouter);
+app.use("/avg", avg);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
